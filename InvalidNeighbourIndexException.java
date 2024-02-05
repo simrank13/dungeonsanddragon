30779db66a6558ea30eabf05f0f09033bf57d3dd
simrank <132793467+simrank13@users.noreply.github.com>
2024-02-05 14:14:07 -0500

    InvalidNeighbourIndexException.java

---
+++ InvalidNeighbourIndexException.java
@@ -0,0 +1,11 @@
+/**
+ * Thrown when access is attempted outside valid Hexagon neighbor index range.
+ * Since it is a Hexagon, can only have 6 neighbors (0-5 inclusive)
+ *
+ */
+public class InvalidNeighbourIndexException extends ArrayIndexOutOfBoundsException{
+
+	public InvalidNeighbourIndexException(int i){
+		super("Invalid index for hexagon neighbor:" + i + "  Must be 0-5 inclusive");
+	}
+}
